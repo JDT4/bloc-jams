f76db58687155b78e92437ceee74763be54bc703
JDT4 <incipientviolet@gmail.com>
2016-01-18 07:06:21 +0000

    Assignment finished

---
+++ scripts/landing.js
@@ -0,0 +1,11 @@
+var points = document.getElementsByClassName('point');   
+var revealPoint = function(index) {
+            points[index].style.opacity = 1;
+            points[index].style.transform = "scaleX(1) translate Y(0)";
+            points[index].style.msTransform = "scaleX(1) translateY(0)";
+            points[index].style.webkitTransform = "scaleX(1) translateY(0)";
+        };
+for (i = 0; i < points.length; i++) {
+revealPoint(i);
+};
+        
