5cb0c014822e42eff0170e4c9b93809be5303239
JDT4 <incipientviolet@gmail.com>
2016-01-23 07:28:02 +0000

    checkpoint finished

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,19 +1,29 @@
 var points = document.getElementsByClassName('point');
-var revealPoint = function(index) {
-            points[index].style.opacity = 1;
-            points[index].style.transform = "scaleX(1) translate Y(0)";
-            points[index].style.msTransform = "scaleX(1) translateY(0)";
-            points[index].style.webkitTransform = "scaleX(1) translateY(0)";
-        };
+var revealPoint = function (index) {
+	points[index].style.opacity = 1;
+	points[index].style.transform = "scaleX(1) translate Y(0)";
+	points[index].style.msTransform = "scaleX(1) translateY(0)";
+	points[index].style.webkitTransform = "scaleX(1) translateY(0)";
+};
+
+//Function 2 does this
 var animatePoints = function () {
-    for (i = 0; i < points.length; i++) {
-revealPoint(i);
+	for (i = 0; i < points.length; i++) {
+		revealPoint(i);
+	}
 };
-};
-window.onload = function () {
-  if (window.innerHeight > 950) {
-      animatePoints(points);
-  } else {
-      animatePoints(points);
-  };
-};
+
+//Thing
+window.onload = function() {
+	     if (window.innerHeight > 950) {
+         animatePoints(revealPoint);
+     }
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener("scroll", function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(revealPoint);   
+         }
+     });
+ };
