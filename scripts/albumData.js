5fa16f8ac551c56f88b7fb447a0a1b5d6b1bf168
JDT4 <incipientviolet@gmail.com>
2016-01-31 07:35:03 +0000

    Checkpoint work

---
+++ scripts/albumData.js
@@ -0,0 +1,83 @@
+//Album 1
+var albumPicasso = {
+	name: 'The Colors',
+	artist: 'Pablo Picasso',
+	label: 'Cubism',
+	year: '1881',
+	albumArtUrl: 'assets/images/album_covers/01.png',
+	songs: [
+		{
+			name: 'Blue',
+			length: '4:26'
+		},
+		{
+			name: 'Green',
+			length: '3:14'
+		},
+		{
+			name: 'Red',
+			length: '5:01'
+		},
+		{
+			name: 'Pink',
+			length: '3:21'
+		},
+		{
+			name: 'Magenta',
+			length: '2:15'
+		}
+     ]
+};
+//Album 2 
+var albumMarconi = {
+	name: 'The Telephone',
+	artist: 'Guglielmo Marconi',
+	label: 'EM',
+	year: '1909',
+	albumArtUrl: 'assets/images/album_covers/20.png',
+	songs: [
+		{
+			name: 'Hello, Operator?',
+			length: '1:01'
+		},
+		{
+			name: 'Ring, ring, ring',
+			length: '5:01'
+		},
+		{
+			name: 'Fits in your pocket',
+			length: '3:21'
+		},
+		{
+			name: 'Can you hear me now?',
+			length: '3:14'
+		},
+		{
+			name: 'Wrong phone number',
+			length: '2:15'
+		}
+     ]
+};
+//Album 3
+var albumKuKaChu = {
+	name: 'KuKaChu',
+	artist: 'KuKa Chuuu',
+	label: 'JB',
+	year: '1999',
+	albumArtUrl: 'assets/images/album_covers/20.png',
+	songs: [
+		{
+			name: 'Divided we stand',
+			length: '3:40'
+		},
+		{
+			name: 'Can\'t believe that',
+			length: '4:12'
+		},
+		{
+			name: 'Bogus',
+			length: '1:20'
+		},
+
+	]
+};
