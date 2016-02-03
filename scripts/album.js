ee5d9f0e78d80a68fd476ec068b7d3b5b2445ae3
JDT4 <incipientviolet@gmail.com>
2016-02-03 20:34:45 +0000

    checkpoint work finished

--- scripts/album.js
+++ scripts/album.js
@@ -87,22 +87,23 @@ var createSongRow = function (songNumber, songName, songLength) {
 	var template =
 		'<tr class="album-view-song-item">' + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>' + '  <td class="song-item-title">' + songName + '</td>' + '  <td class="song-item-duration">' + songLength + '</td>' + '</tr>';
 
-	return template;
+	return $(template);
 };
 // Set album info
 var setCurrentAlbum = function (album) {
-	var albumTitle = document.getElementsByClassName('album-view-title')[0];
-	var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-	var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-	var albumImage = document.getElementsByClassName('album-cover-art')[0];
-	var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-	albumTitle.firstChild.nodeValue = album.name;
-	albumArtist.firstChild.nodeValue = album.artist;
-	albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-	albumImage.setAttribute('src', album.albumArtUrl);
-	albumSongList.innerHTML = '';
+	var $albumTitle = $('.album-view-title');
+	var $albumArtist = $('.album-view-artist');
+	var $albumReleaseInfo = $('.album-view-release-info');
+	var $albumImage = $('.album-cover-art');
+	var $albumSongList = $('.album-view-song-list');
+	$albumTitle.text(album.name);
+	$albumArtist.text(album.artist);
+	$albumReleaseInfo.text(album.year + ' ' + album.label);
+	$albumImage.attr('src', album.albumArtUrl);
+	$albumSongList.empty();
 	for (i = 0; i < album.songs.length; i++) {
-		albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
+		var $newRow = createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
+		$albumSongList.append($newRow);
 	}
 };
 
@@ -143,20 +144,6 @@ var playLeave = function () {
 };
 
 //Find song-item-number class refactor my solution
-/*var findParentByClassName = function (element, targetClass) {
-		var currentParent = element.parentElement;
-		if (currentParent) {
-			while (currentParent.className != targetClass) {
-				currentParent = currentParent.parentElement;
-			}
-			if (currentParent == targetClass) {
-				return currentParent;
-			} else if (currentParent == window) {
-				alert("No parent found with that class name")
-			} else {
-				alert("No parent found")
-			}
-		};*/
 
 // Find song-item-number class bloc solution
 var findParentByClassName = function (element, targetClass) {
